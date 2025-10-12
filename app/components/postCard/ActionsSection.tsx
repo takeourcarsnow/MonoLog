0f989fd35166fa8d79e82ed628f85c50b154c200
takeourcarsnow <ignasnefas@gmail.com>
2025-10-12 20:25:15 +0300

    fix(ActionSection): reduce SVG icon size for better alignment and aesthetics refactor(cards): clean up hover styles and remove unused media queries

--- app/components/postCard/ActionsSection.tsx
+++ app/components/postCard/ActionsSection.tsx
@@ -176,8 +176,8 @@ export const ActionsSection = function ActionsSection({
           }}
         >
           <svg
-            width="16"
-            height="16"
+            width="12"
+            height="12"
             viewBox="0 0 24 24"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"

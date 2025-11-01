302e8ef516a21ca512b2edd13ba68e4100b925e6
takeourcarsnow <ignasnefas@gmail.com>
2025-11-01 23:37:02 +0200

    refactor(uploader,components): add barrel index to re-export UploaderAuthWrapper, UploaderCore and UI subcomponents (ImageEditorModal, PhotoActionRow, SizeWarning)

---
+++ app/components/uploader/components/index.ts
@@ -0,0 +1,8 @@
+// Main uploader components
+export { UploaderAuthWrapper } from './UploaderAuthWrapper';
+export { UploaderCore } from './UploaderCore';
+
+// UI Components
+export { ImageEditorModal } from './ImageEditorModal';
+export { PhotoActionRow } from './PhotoActionRow';
+export { SizeWarning } from './SizeWarning';
